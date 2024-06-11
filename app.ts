be407f91fc379bc8ab06070bfc1e74d0cf2bc7da
Artiom-Evs <artiom.evs@gmail.com>
2024-06-11 17:24:42 +0200

    Implement basic express.js API

--- app.ts
+++ app.ts
@@ -1,1 +1,10 @@
-console.log('Hello world');
+import express from "express";
+
+const app = express();
+const PORT = 5000;
+
+app.get("/", (req, res) => {
+    res.send("I am working!");
+});
+
+app.listen(PORT, () => console.log(`Running on port ${PORT}`));
