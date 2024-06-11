31674054e9a35575343e2a072e4871d4be3579f5
Artiom-Evs <artiom.evs@gmail.com>
2024-06-11 22:01:29 +0200

    Add domain data model schemas
    schemas.ts file contains all data model types which represents WooCommerce REST API response data objects.

---
+++ schemas.ts
@@ -0,0 +1,71 @@
+import { z } from "zod";
+
+export type Product = z.infer<typeof ProductSchema>;
+export type Variation = z.infer<typeof VariationSchema>;
+export type Image = z.infer<typeof ImageSchema>;
+export type Category = z.infer<typeof CategorySchema>;
+export type ProductAttribute = z.infer<typeof ProductAttributeSchema>;
+export type VariationAttribute = z.infer<typeof VariationAttributeSchema>;
+
+export const ImageSchema = z.object({
+    id: z.number(),
+    name: z.string(),
+    src: z.string().url()
+});
+
+export const CategorySchema = z.object({
+    id: z.number(),
+    parent_id: z.number(),
+    name: z.string().min(1),
+    slug: z.string().min(1),
+    description: z.string(),
+    count: z.number()
+});
+
+export const VariationAttributeSchema = z.object({
+    id: z.number(),
+    name: z.string().min(1),
+    option: z.string().min(1)
+});
+
+export const ProductAttributeSchema = z.object({
+    id: z.number(),
+    name: z.string().min(1),
+    slug: z.string().min(1),
+    visible: z.boolean(),    
+    variation: z.boolean(),
+    options: z.array(VariationAttributeSchema)
+});
+
+export const VariationSchema = z.object({
+    id: z.number(),
+    parent_id: z.number(),
+    sku: z.string().min(1),
+    name: z.string().min(1),
+    slug: z.string().min(1),
+    description: z.string(),
+    stock_quantity: z.number(),
+    price: z.number(),
+    created: z.date(),
+    modified: z.date(),
+    images: z.array(ImageSchema),
+    attributes: z.array(VariationAttributeSchema)
+});
+
+export const ProductSchema = z.object({
+    id: z.number(),
+    sku: z.string().min(1),
+    name: z.string().min(1),
+    slug: z.string().min(1),
+    description: z.string(),
+    type: z.enum([ "simple", "variable" ]),
+    stock_quantity: z.number(),
+    price: z.number(),
+    created: z.date(),
+    modified: z.date(),
+    categories: z.array(CategorySchema),
+    images: z.array(ImageSchema),
+    attributes: z.array(ProductAttributeSchema),
+    default_attribute: z.array(VariationAttributeSchema),
+    variations: z.array(VariationSchema)
+});
