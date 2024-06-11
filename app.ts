f1fbe9ba75a911791efa5743f20248893fa18efa
Artiom-Evs <artiom.evs@gmail.com>
2024-06-11 17:58:42 +0200

    Implement basic API with integrated validation
    Validation was implemented using express-zod-api library.

--- app.ts
+++ app.ts
@@ -1,10 +1,35 @@
-import express from "express";
+import { z } from "zod";
+import { createConfig, defaultEndpointsFactory } from "express-zod-api";
+import { Routing } from "express-zod-api";
+import { createServer } from "express-zod-api";
 
-const app = express();
-const PORT = 5000;
-
-app.get("/", (req, res) => {
-    res.send("I am working!");
+const config = createConfig({
+    server: {
+        listen: 8090, // port, UNIX socket or options
+    },
+    cors: true,
+    logger: { level: "debug", color: true },
 });
 
-app.listen(PORT, () => console.log(`Running on port ${PORT}`));
+
+const helloApi = defaultEndpointsFactory.build({
+    method: "get",
+    input: z.object({
+        name: z.string().default("anonimous")
+    }),
+    output: z.object({
+        message: z.string()
+    }),
+    handler: async ({ input: { name }, options, logger }) => {
+        logger.debug("Options:", options);
+        return { message: `Hello, ${name || "World"}. Happy coding!` };
+    },
+});
+
+const routing: Routing = {
+    v1: {
+        hello: helloApi
+    }
+};
+
+createServer(config, routing);
