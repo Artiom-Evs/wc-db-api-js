9bd9534648d78264b382a1b0c42a04cf08b901b2
Artiom-Evs <artiom.evs@gmail.com>
2024-08-12 17:49:06 +0200

    Implement getting post thumbnail images in the posts API endpoints

--- infrastructure/PostsRepository.ts
+++ infrastructure/PostsRepository.ts
@@ -1,17 +1,20 @@
 import { Post } from "../schemas";
 import categoriesRepository from "./CategoriesRepository";
 import pool from "./DbConnectionPool";
+import imagesRepository from "./ImagesRepository";
 import RepositoryBase from "./RepositoryBase";
 
 const GET_ALL_QUERY = `
-select ID AS id, post_author AS author, post_content AS content, post_title AS title, post_excerpt AS excerpt, post_status AS status, post_name AS slug, post_parent AS parent, menu_order, post_type AS type
-from wp_posts
+select ID AS id, post_author AS author, post_content AS content, post_title AS title, post_excerpt AS excerpt, post_status AS status, post_name AS slug, post_parent AS parent, menu_order, post_type AS type, post_date AS created, post_modified AS modified, CAST(pm.meta_value AS INT) AS thumbnail_id
+from wp_posts AS p
+LEFT JOIN wp_postmeta AS pm ON p.ID = pm.post_id AND pm.meta_key = "_thumbnail_id"
 where post_type = "post" AND post_name != "";
 `;
 
 const GET_BY_SLUG_QUERY = `
-select ID AS id, post_author AS author, post_content AS content, post_title AS title, post_excerpt AS excerpt, post_status AS status, post_name AS slug, post_parent AS parent, menu_order, post_type AS type
-from wp_posts
+select ID AS id, post_author AS author, post_content AS content, post_title AS title, post_excerpt AS excerpt, post_status AS status, post_name AS slug, post_parent AS parent, menu_order, post_type AS type, post_date AS created, post_modified AS modified, CAST(pm.meta_value AS INT) AS thumbnail_id
+from wp_posts AS p
+LEFT JOIN wp_postmeta AS pm ON p.ID = pm.post_id AND pm.meta_key = "_thumbnail_id"
 where post_type = "post"
     AND post_name = ?
 LIMIT 1;
@@ -20,13 +23,18 @@ LIMIT 1;
 class PostsRepository extends RepositoryBase {
     public async getAll(): Promise<Post[]> {
         const [rows] = await this._pool.execute<any[]>(GET_ALL_QUERY, []);
+        const thumbnailIds = rows.map(r => r.thumbnail_id ?? 0);
         const posts = rows as Post[];
+        const images = await imagesRepository.getImagesByIds(thumbnailIds, "medium");
+
+        console.debug("IMAGES:", posts);
 
         const postIds = posts.map(p => p.id);
         const categories = await categoriesRepository.getPostsCategories(postIds);
 
         posts.forEach(post => {
             post.categories = categories.filter(c => c.object_id == post.id);
+            post.thumbnail = images.find(i => i.id === (post as any).thumbnail_id)?.src ?? null;
         });
 
         return posts;
@@ -34,6 +42,8 @@ class PostsRepository extends RepositoryBase {
 
     public async getBySlug(slug: string): Promise<Post | null> {
         const [[row]] = await this._pool.execute<any[]>(GET_BY_SLUG_QUERY, [slug]);
+        const [image] = await imagesRepository.getImagesByIds([ row.thumbnail_id ?? 0 ], "large");
+
         const post = row as Post ?? null;
 
         if (!post)
@@ -41,6 +51,7 @@ class PostsRepository extends RepositoryBase {
 
         const categories = await categoriesRepository.getPostsCategories([post.id]);
         post.categories = categories;
+        post.thumbnail = image?.src ?? null;
 
         return post;
     }
