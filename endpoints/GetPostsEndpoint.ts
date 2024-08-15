847d3a0ce82248ed45b4d34889eb0a946a1ede45
Artiom-Evs <artiom.evs@gmail.com>
2024-08-15 11:24:04 +0200

    Implement pagination for the GetPosts API endpoint

--- endpoints/GetPostsEndpoint.ts
+++ endpoints/GetPostsEndpoint.ts
@@ -5,14 +5,17 @@ import postsRepository from "../infrastructure/PostsRepository";
 
 export const GetPostsEndpoint = defaultEndpointsFactory.build({
     method: "get",
-    input: z.object({}),
+    input: z.object({
+        page: z.string().optional().default("1").transform(s => parseInt(s)),
+        per_page: z.string().optional().default("100").transform(s => parseInt(s))
+    }),
     output: z.object({
         items: z.array(PostSchema)
     }),
     handler: async ({ input, options, logger }) => {
         logger.debug("Requested parameters:", input);
 
-        const posts = await postsRepository.getAll();
+        const posts = await postsRepository.getAll(input);
 
         return { items: posts };
     },
