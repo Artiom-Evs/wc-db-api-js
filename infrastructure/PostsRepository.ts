4d9dcc5f688db56ce4a8c5f93d30a59cf1a16cac
Artiom-Evs <artiom.evs@gmail.com>
2024-08-16 11:18:57 +0200

    Implement returning previous and next post slugs for the GetPostBySlug API endpoint

--- infrastructure/PostsRepository.ts
+++ infrastructure/PostsRepository.ts
@@ -20,11 +20,20 @@ where post_type = "post" AND post_name != "";
 `;
 
 const GET_BY_SLUG_QUERY = `
-select ID AS id, post_author AS author, post_content AS content, post_title AS title, post_excerpt AS excerpt, post_status AS status, post_name AS slug, post_parent AS parent, menu_order, post_type AS type, post_date AS created, post_modified AS modified, CAST(pm.meta_value AS INT) AS thumbnail_id
+select ID AS id, post_author AS author, post_content AS content, post_title AS title, post_excerpt AS excerpt, post_status AS status, post_name AS slug, post_parent AS parent, menu_order, post_type AS type, post_date AS created, post_modified AS modified, CAST(pm.meta_value AS INT) AS thumbnail_id,
+    (SELECT post_name FROM wp_posts AS np
+    where np.post_type = "post" AND post_name != "" AND np.post_date < p.post_date
+    ORDER BY np.post_date DESC
+    LIMIT 1) AS prev_post,
+    (SELECT post_name FROM wp_posts AS np
+    where np.post_type = "post" AND post_name != "" AND np.post_date > p.post_date
+    ORDER BY np.post_date
+    LIMIT 1) AS next_post
 from wp_posts AS p
 LEFT JOIN wp_postmeta AS pm ON p.ID = pm.post_id AND pm.meta_key = "_thumbnail_id"
-where post_type = "post"
-    AND post_name = ?
+where post_type = "post" AND post_name != ""
+    AND p.post_name = ?
+ORDER BY post_date
 LIMIT 1;
 `;
 
